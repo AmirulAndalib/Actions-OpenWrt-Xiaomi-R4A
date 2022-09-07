f3486166b5583e32a9cddba8be18e8c637abd84f
AmirulAndalib <amirulandalib9@gmail.com>
2022-09-07 13:53:24 +0600

    d

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,3 +18,17 @@ sed -i "s/OpenWrt /ababwnq build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" pack
 
 # 修改默认wifi名称ssid为Xiaomi_R4A
 sed -i 's/ssid=OpenWrt/ssid=Xiaomi_R4A/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# Install Luci Theme Neobird
+git clone https://github.com/thinktip/luci-theme-neobird package/luci-theme-neobird
+
+default_theme='Neobird'	
+sed -i "s/bootstrap/$default_theme/g" feeds/luci/modules/luci-base/root/etc/config/luci
+
+# change lang from CN to EN
+sed -i "s/zh_cn/en/g" package/lean/default-settings/files/zzz-default-settings
+sed -i "s/+@LUCI_LANG_zh-cn/+@LUCI_LANG_en/g" package/lean/default-settings/Makefile
+sed -i "/po2lmo ./po/zh-cn/default.po/d" package/lean/default-settings/Makefile
+
+# Install AdguardHome
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/feeds/luci/luci-app-adguardhome
